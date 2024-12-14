96cc7b311401819bc9aaba6d81bce820888f3a8b
Rushikeshkapare <rushikesh.kapare11@.com>
2024-12-14 12:12:36 +0530

    file chnages

--- src/practice/Practice3.java
+++ src/practice/Practice3.java
@@ -5,5 +5,7 @@ public class Practice3 {
 	public static void main (String[] args) {
 		System.out.println("Hello_Class1 Branch A chnages");
 		System.out.println("Branch B chnages");
+		System.out.println("file chnages");
+		System.out.println("chnages in branch B");
 	}
 }
--- src/practice/Practice4.java
+++ src/practice/Practice4.java
@@ -5,5 +5,7 @@ public class Practice4 {
 	public static void main (String[] args) {
 		System.out.println("Hello_Class2 Branch A chnages");
 		System.out.println("Branch B chnages");
+		System.out.println("file chnages");
+		System.out.println("chnages in branch B");
 	}
 }
