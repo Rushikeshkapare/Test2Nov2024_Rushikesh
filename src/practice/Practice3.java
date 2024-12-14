fc0cee6e429d87b40c7add562bdfb738b1519172
Rushikeshkapare <rushikesh.kapare11@.com>
2024-12-14 11:32:57 +0530

    Add changes

--- src/practice/Practice3.java
+++ src/practice/Practice3.java
@@ -3,6 +3,7 @@ package practice;
 public class Practice3 {
 	
 	public static void main (String[] args) {
-		System.out.println("Hello_Class1 Branch A chnages");
+		System.out.println("Hello_Class1");
+		System.out.println("Branch A");
 	}
 }
