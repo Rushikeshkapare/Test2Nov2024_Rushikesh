157b01934a09446f0da01763707fe61d84ad5236
Rushikeshkapare <rushikesh.kapare11@.com>
2024-12-29 12:15:34 +0530

    Commit-3

--- src/practice/Calculator1.java
+++ src/practice/Calculator1.java
@@ -4,7 +4,9 @@ public class Calculator1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Commit-3");
+		System.out.println("commit-4");
+		System.out.println("Commit-5");
 	}
 
 }
--- src/practice/Calculator2.java
+++ src/practice/Calculator2.java
@@ -4,7 +4,9 @@ public class Calculator2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Commit-3");
+		System.out.println("commit-4");
+		System.out.println("Commit-5");
 	}
 
 }
